52526c0991c7850efbbbc1c4226e0ca0f06c3625
Abderrahmane Zerouali <abderrahmanezerouali98@gmail.com>
2023-09-13 16:44:31 -0700

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """prints all the names defined by the compiled module hidden_4"""
+    """prints all the names defined by hidden_4"""
     import hidden_4
     names = dir(hidden_4)
 
